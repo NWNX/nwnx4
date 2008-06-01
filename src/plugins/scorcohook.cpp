e2e4c2f90a03235dff70247a303744ff171627f7
virusman <virusman@virusman.ru>
2008-06-01 22:46:57 +0000

    Fixed SCO/RCO hook

--- src/plugins/scorcohook.cpp
+++ src/plugins/scorcohook.cpp
@@ -31,7 +31,7 @@ int __stdcall SCOHookProc(char** database, char** key, char** player, int flags,
 {
 	int *pThis;
 	__asm {mov pThis, ecx}
-	if(!*pThis) return 0;
+	if(!pThis) return 0;
 
 	if (memcmp(*database, "NWNX", 4))
 	{
@@ -48,7 +48,7 @@ unsigned char * __stdcall RCOHookProc(char** database, char** key, char** player
 {
 	int *pThis;
 	__asm {mov pThis, ecx}
-	if(!*pThis) return NULL;
+	if(!pThis) return NULL;
 
 	if (memcmp(*database, "NWNX", 4))
 	{
@@ -64,25 +64,43 @@ unsigned char * __stdcall RCOHookProc(char** database, char** key, char** player
 
 DWORD FindHookSCO()
 {
+	//8B 44 24 04 56 57 6A 01 50 8B F9 E8 ** ** ** ** 8B F0 85 F6 75 07 5F 33 C0 5E C2 18 00 8B 54 24 14
 	char* ptr = (char*) 0x400000;
 	while (ptr < (char*) 0x800000)
 	{
-		if ((ptr[0] == (char) 0x8b) &&
-			(ptr[1] == (char) 0x09) &&
-			(ptr[2] == (char) 0x85) &&
-			(ptr[3] == (char) 0xc9) &&
-			(ptr[4] == (char) 0x74) &&
-			(ptr[5] == (char) 0x05) &&
-			(ptr[6] == (char) 0xE9) &&
-			(ptr[7] == (char) 0xE5) &&
-			(ptr[8] == (char) 0x12) &&
-			(ptr[9] == (char) 0x00) &&
-			(ptr[0xA] == (char) 0x00) &&
-			(ptr[0xB] == (char) 0x33) &&
-			(ptr[0xC] == (char) 0xC0) &&
-			(ptr[0xD] == (char) 0xC2) &&
-			(ptr[0xE] == (char) 0x18) &&
-			(ptr[0xF] == (char) 0x00)
+		if ((ptr[0] == (char) 0x8B) &&
+			(ptr[1] == (char) 0x44) &&
+			(ptr[2] == (char) 0x24) &&
+			(ptr[3] == (char) 0x04) &&
+			(ptr[4] == (char) 0x56) &&
+			(ptr[5] == (char) 0x57) &&
+			(ptr[6] == (char) 0x6A) &&
+			(ptr[7] == (char) 0x01) &&
+			(ptr[8] == (char) 0x50) &&
+			(ptr[9] == (char) 0x8B) &&
+			(ptr[0xA] == (char) 0xF9) &&
+			(ptr[0xB] == (char) 0xE8) &&
+			//c
+			//d
+			//e
+			//f
+			(ptr[0x10] == (char) 0x8B) &&
+			(ptr[0x11] == (char) 0xF0) &&
+			(ptr[0x12] == (char) 0x85) &&
+			(ptr[0x13] == (char) 0xF6) &&
+			(ptr[0x14] == (char) 0x75) &&
+			(ptr[0x15] == (char) 0x07) &&
+			(ptr[0x16] == (char) 0x5F) &&
+			(ptr[0x17] == (char) 0x33) &&
+			(ptr[0x18] == (char) 0xC0) &&
+			(ptr[0x19] == (char) 0x5E) &&
+			(ptr[0x1A] == (char) 0xC2) &&
+			(ptr[0x1B] == (char) 0x18) &&
+			(ptr[0x1C] == (char) 0x00) &&
+			(ptr[0x1D] == (char) 0x8B) &&
+			(ptr[0x1E] == (char) 0x54) &&
+			(ptr[0x1F] == (char) 0x24) &&
+			(ptr[0x20] == (char) 0x14)
 			)
 			return (DWORD) ptr;
 		else
@@ -93,24 +111,36 @@ DWORD FindHookSCO()
 
 DWORD FindHookRCO()
 {
+	//8B 44 24 04 56 57 33 FF 57 50 8B F1 E8 ** ** ** ** 85 C0 0F 84 9C 00 00 00 8B 4C 24 14 8B 54 24 10 51 52 50
 	char* ptr = (char*) 0x400000;
 	while (ptr < (char*) 0x800000)
 	{
-		if ((ptr[0] == (char) 0x8b) &&
-			(ptr[1] == (char) 0x09) &&
-			(ptr[2] == (char) 0x85) &&
-			(ptr[3] == (char) 0xc9) &&
-			(ptr[4] == (char) 0x74) &&
-			(ptr[6] == (char) 0xE9) &&
-			(ptr[7] == (char) 0xB5) &&
-			(ptr[8] == (char) 0x0F) &&
-			(ptr[9] == (char) 0x00) &&
-			(ptr[0xA] == (char) 0x00) &&
-			(ptr[0xB] == (char) 0x33) &&
-			(ptr[0xC] == (char) 0xC0) &&
-			(ptr[0xD] == (char) 0xC2) &&
-			(ptr[0xE] == (char) 0x14) &&
-			(ptr[0xF] == (char) 0x00)
+		if ((ptr[0] == (char) 0x8B) &&
+			(ptr[1] == (char) 0x44) &&
+			(ptr[2] == (char) 0x24) &&
+			(ptr[3] == (char) 0x04) &&
+			(ptr[4] == (char) 0x56) &&
+			(ptr[5] == (char) 0x57) &&
+			(ptr[6] == (char) 0x33) &&
+			(ptr[7] == (char) 0xFF) &&
+			(ptr[8] == (char) 0x57) &&
+			(ptr[9] == (char) 0x50) &&
+			(ptr[0xA] == (char) 0x8B) &&
+			(ptr[0xB] == (char) 0xF1) &&
+			(ptr[0xC] == (char) 0xE8) &&
+			//d
+			//e
+			//f
+			//10
+			(ptr[0x11] == (char) 0x85) &&
+			(ptr[0x12] == (char) 0xC0) &&
+			(ptr[0x13] == (char) 0x0F) &&
+			(ptr[0x14] == (char) 0x84) &&
+			(ptr[0x15] == (char) 0x9C) &&
+			(ptr[0x16] == (char) 0x00) &&
+			(ptr[0x17] == (char) 0x00) &&
+			(ptr[0x18] == (char) 0x00) &&
+			(ptr[0x19] == (char) 0x8B)
 			)
 			return (DWORD) ptr;
 		else
@@ -120,8 +150,10 @@ DWORD FindHookRCO()
 }
 
 int HookSCORCO()
-{
-	int sco_success, rco_success;
+{	
+	DetourTransactionBegin();
+	DetourUpdateThread(GetCurrentThread());
+	int sco_success, rco_success, detour_success;
 	DWORD sco = FindHookSCO();
 	if (sco)
 	{
@@ -129,7 +161,7 @@ int HookSCORCO()
 		//odmbc.Log(0, "o SCO located at %x.\n", sco);
 		//sco_success = HookCode((PVOID) sco, SCOHookProc, (PVOID*) &OriginalSCO);
 		*(DWORD*)&OriginalSCO = sco;
-		sco_success = DetourAttach(&(PVOID&)OriginalSCO, SCOHookProc);
+		sco_success = DetourAttach(&(PVOID&)OriginalSCO, SCOHookProc)==0;
 	}
 	else
 	{
@@ -143,13 +175,14 @@ int HookSCORCO()
 		wxLogMessage(wxT("o RCO located at %x."), rco);
 		//rco_success = HookCode((PVOID) rco, RCOHookProc, (PVOID*) &OriginalRCO);
 		*(DWORD*)&OriginalRCO = rco;
-		rco_success = DetourAttach(&(PVOID&)OriginalRCO, RCOHookProc);
+		rco_success = DetourAttach(&(PVOID&)OriginalRCO, RCOHookProc)==0;
 	}
 	else
 	{
 		wxLogMessage(wxT("! RCO locate failed."));
 		return 0;
 	}
-	return sco_success&&rco_success;
+    detour_success = DetourTransactionCommit()==0;
+	return sco_success&&rco_success&&detour_success;
 }
 
