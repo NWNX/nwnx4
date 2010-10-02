9441628ca92b5e2c7d497dcf4f7da3dd345f4a99
Zebranky <andrew@mercuric.net>
2010-10-02 03:22:48 +0000

    Quick semi-hacky funcs plugin to provide GETSOUNDSET.

---
+++ src/plugins/funcs/funcs.cpp
@@ -0,0 +1,135 @@
+/***************************************************************************
+    NWNX Funcs - Various functions plugin
+    Copyright (C) 2010 Andrew Brockert (Zebranky, andrew@mercuric.net) 
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ***************************************************************************/
+
+#include "funcs.h"
+
+/***************************************************************************
+    NWNX and DLL specific functions
+***************************************************************************/
+
+Funcs* plugin;
+
+typedef unsigned long objid_t;
+typedef unsigned long dword;
+void ** pCAppManager = (void **)0x0086442C;
+void * (__fastcall *CServerExoApp__GetCreatureByGameObjectID)(void *, void *, objid_t) = (void * (__fastcall *)(void *, void *, objid_t))0x0054A1B0;
+unsigned short (__fastcall *CNWSCreature__GetSoundSet)(void *) = (unsigned short (__fastcall *)(void *))0x005504A0;
+
+DLLEXPORT Plugin* GetPluginPointerV2()
+{
+	return plugin;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+	{
+		plugin = new Funcs();
+
+		TCHAR szPath[MAX_PATH];
+		GetModuleFileName(hModule, szPath, MAX_PATH);
+		plugin->SetPluginFullPath(szPath);
+	}
+	else if (ul_reason_for_call == DLL_PROCESS_DETACH)
+	{
+		delete plugin;
+	}
+    return TRUE;
+}
+
+
+/***************************************************************************
+    Implementation of Funcs Plugin
+***************************************************************************/
+
+Funcs::Funcs()
+{
+	header = _T(
+		"NWNX Funcs Plugin V.0.0.1\n" \
+		"(c) 2010 Andrew Brockert (Zebranky)\n" \
+		"visit us at http://www.nwnx.org\n");
+
+	description = _T(
+		"This plugin provides functions to poke at NWN2 internals.");
+
+	subClass = _T("FUNCS");
+	version = _T("0.0.1");
+}
+
+Funcs::~Funcs()
+{
+	wxLogMessage(wxT("* Plugin unloaded."));
+}
+
+bool Funcs::Init(TCHAR* nwnxhome)
+{
+	assert(GetPluginFileName());
+
+	/* Log file */
+	wxString logfile(nwnxhome); 
+	logfile.append(wxT("\\"));
+	logfile.append(GetPluginFileName());
+	logfile.append(wxT(".txt"));
+	logger = new wxLogNWNX(logfile, wxString(header.c_str()));
+
+	wxLogMessage(wxT("* Plugin initialized."));
+	return true;
+}
+
+void Funcs::GetFunctionClass(TCHAR* fClass)
+{
+	_tcsncpy_s(fClass, 128, wxT("FUNCS"), 5); 
+}
+
+int Funcs::GetInt(char* sFunction, char* sParam1, int nParam2)
+{
+	wxLogTrace(TRACE_VERBOSE, wxT("* Plugin GetInt(0x%x, %s, %d)"), 0x0, sParam1, nParam2);
+
+#ifdef UNICODE
+	wxString wxRequest(sFunction, wxConvUTF8);
+	wxString function(sFunction, wxConvUTF8);
+#else
+	wxString wxRequest(sFunction);
+	wxString function(sFunction);
+#endif
+
+	if (function == wxT(""))
+	{
+		wxLogMessage(wxT("* Function not specified."));
+		return NULL;
+	}
+	else if(function == wxT("GETSOUNDSET"))
+	{
+		objid_t creature_oid = nParam2;
+		void *cre = CServerExoApp__GetCreatureByGameObjectID(*pCAppManager, NULL, creature_oid);
+		if(cre != NULL)
+		{
+			return CNWSCreature__GetSoundSet(cre);
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	else // unknown function
+	{
+		return 0;
+	}
+}
+
---
+++ src/plugins/funcs/funcs.h
@@ -0,0 +1,51 @@
+/***************************************************************************
+    NWNX Funcs - Various functions plugin
+    Copyright (C) 2010 Andrew Brockert (Zebranky, andrew@mercuric.net) 
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ ***************************************************************************/
+#if !defined(FUNCS_H_INCLUDED)
+#define FUNCS_H_INCLUDED
+
+#define DLLEXPORT extern "C" __declspec(dllexport)
+
+#include "windows.h"
+#include "../plugin.h"
+#include "../../misc/log.h"
+#include "wx/tokenzr.h"
+#include "wx/hashset.h"
+
+class Funcs : public Plugin
+{
+public:
+	Funcs();
+	~Funcs();
+
+	bool Init(TCHAR* nwnxhome);  
+
+	int GetInt(char* sFunction, char* sParam1, int nParam2);
+	void SetInt(char* sFunction, char* sParam1, int nParam2, int nValue) {};
+	float GetFloat(char* sFunction, char* sParam1, int nParam2) { return 0.0; }
+	void SetFloat(char* sFunction, char* sParam1, int nParam2, float fValue) {};
+	void SetString(char* sFunction, char* sParam1, int nParam2, char* sValue) { return; }
+	char* GetString(char* sFunction, char* sParam1, int nParam2) { return NULL; }
+	void GetFunctionClass(TCHAR* fClass);
+
+private:
+	wxLogNWNX* logger;
+};
+
+#endif
