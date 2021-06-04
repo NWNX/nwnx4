6b15d3623c57dc5065221c6af464fe93b3a4e36d
Scott Munday <scottmunday84@gmail.com>
2021-06-04 17:04:59 -0400

    Added missing files.

---
+++ src/gui/stdwx.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Adriana on 6/4/2021.
+//
+
+#include "stdwx.h"
---
+++ src/gui/stdwx.h
@@ -0,0 +1,16 @@
+// http://www.litwindow.com/Knowhow/wxHowto/wxhowto.html#precompiled
+
+#include "wx/wx.h" 
+#include "wx/mstream.h"
+#include "wx/image.h"
+
+// debug memory allocation enhancement (see next tip)
+#ifdef _DEBUG
+#include <crtdbg.h>
+#define DEBUG_NEW new(_NORMAL_BLOCK ,__FILE__, __LINE__)
+#else
+#define DEBUG_NEW new
+#endif
+
+// #pragma warning (disable:4786)
+
